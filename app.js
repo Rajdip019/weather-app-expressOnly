3b6c49f63566f97d87838dbcdb41d3f38c633347
Rajdeep Sengupta <rajdipgupta019@gmail.com>
2021-11-16 21:40:12 +0530

    Weather App V 1.3 (Added Temp feels like & Pressure)

--- app.js
+++ app.js
@@ -25,11 +25,13 @@ app.post("/", function (req, res) {
         console.log(response.statusCode);
         response.on("data", function (data) {
             const weatherData = JSON.parse(data)
-            const temp = weatherData.main.feels_like;
+            const temp = weatherData.main.temp;
+            const feels_like = weatherData.main.feels_like;
             const icon = weatherData.weather[0].icon;
             const temp_min = weatherData.main.temp_min;
             const temp_max = weatherData.main.temp_max;
             const humidity=weatherData.main.humidity;
+            const pressure=weatherData.main.pressure;
             const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
             res.setHeader("Content-Type", "text/html");
             res.write('<head><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet"></head>')
@@ -37,9 +39,9 @@ app.post("/", function (req, res) {
             res.write("<style>header{text-align: center; font-size: 4rem; padding: 1rem;background: rgba(0, 247, 255, 0.123); box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );backdrop-filter: blur( 6px ); -webkit-backdrop-filter: blur( 6px );border-radius: 10px;color: black;font-weight: bolder; }</style>")
             res.write("<style>.form{background: rgba( 255, 255, 255, 0.4 ); box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );backdrop-filter: blur( 6px );-webkit-backdrop-filter: blur( 6px );border-radius: 10px; max-width: 50rem; margin-left: auto;margin-right: auto;padding: 2rem; }.form h1{padding: 1rem 0rem}</style>")
             res.write("<style>.main{height: 49.55vh;text-align: center; justify-content: center;padding: 10rem 0;}</style>")
-            res.write("<style>footer{text-align: center;background-color: black; color: white; height: 5vh;line-height: 5vh; }</style>");
+            res.write("<style>.min-max{width: 25rem}.flex{display: flex;}h3{padding: 0.5rem; }footer{text-align: center;background-color: black; color: white; height: 5vh;line-height: 5vh; }</style>");
             res.write("<header> Weather App</header>");
-            res.write("<div class='main'><div class='form'><h1>The temperature in "+palce+ " is: " + temp + " Degree celcius.</h1><h3>The Weather Currently is: " + weatherData.weather[0].description+ ". </h3><h3>Min Temp: "+temp_min+ " celcius</h3><h3>Max Temp: "+temp_max+ " celcius</h3><h3>Humidity: "+humidity+ "%</h3><img src=" + imageURL + "></div></div>");
+            res.write("<div class='main'><div class='form'><h1>The temperature in "+palce+ " is: " + temp + " Degree celcius.</h1><h3>The Temp Feels like: "+feels_like+"</h3><h3>The Weather Currently is: " + weatherData.weather[0].description+ ". </h3><img src=" + imageURL + "><div class='flex'><div class='min-max'><h3>Min Temp: "+temp_min+ " celcius</h3></div><div class='min-max'><h3>Max Temp: "+temp_max+ " celcius</h3></div></div><div class='flex'><div class='min-max'><h3>Humidity: "+humidity+ "%</h3></div><div class='min-max'><h3>Presure: "+pressure+ "bar</h3></div></div></div></div>");
             res.write("<footer>Copyright &copy; 2021 | Build by Rajdeep Sengupta with Love.</footer>")
             res.send();
         })
