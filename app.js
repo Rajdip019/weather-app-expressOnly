e6b9d5ef66c3e05cd10ccc42a519d00b479c1494
Rajdeep Sengupta <rajdipgupta019@gmail.com>
2021-11-16 15:49:04 +0530

    Weather App V-1.2 Min/Max Temp & Humidity Added

--- app.js
+++ app.js
@@ -27,6 +27,9 @@ app.post("/", function (req, res) {
             const weatherData = JSON.parse(data)
             const temp = weatherData.main.feels_like;
             const icon = weatherData.weather[0].icon;
+            const temp_min = weatherData.main.temp_min;
+            const temp_max = weatherData.main.temp_max;
+            const humidity=weatherData.main.humidity;
             const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
             res.setHeader("Content-Type", "text/html");
             res.write('<head><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet"></head>')
@@ -36,7 +39,7 @@ app.post("/", function (req, res) {
             res.write("<style>.main{height: 49.55vh;text-align: center; justify-content: center;padding: 10rem 0;}</style>")
             res.write("<style>footer{text-align: center;background-color: black; color: white; height: 5vh;line-height: 5vh; }</style>");
             res.write("<header> Weather App</header>");
-            res.write("<div class='main'><div class='form'><h1>The temperature in "+palce+ " is: " + temp + " Degree celcius.</h1><h3>The Weather Currently is: " + weatherData.weather[0].description+ ". </h3><img src=" + imageURL + "></div></div>");
+            res.write("<div class='main'><div class='form'><h1>The temperature in "+palce+ " is: " + temp + " Degree celcius.</h1><h3>The Weather Currently is: " + weatherData.weather[0].description+ ". </h3><h3>Min Temp: "+temp_min+ " celcius</h3><h3>Max Temp: "+temp_max+ " celcius</h3><h3>Humidity: "+humidity+ "%</h3><img src=" + imageURL + "></div></div>");
             res.write("<footer>Copyright &copy; 2021 | Build by Rajdeep Sengupta with Love.</footer>")
             res.send();
         })
