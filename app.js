e26850289ef8f4f0c28932b31d7a660e10b407ea
Rajdeep Sengupta <rajdipgupta019@gmail.com>
2021-11-20 14:44:30 +0530

    Wather App V-1.8.2 Popular Cities Initialized.

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ app.use(express.static('image'))
 app.get("/", function (req, res) {
 
     res.sendFile(__dirname + "/index.html");
-
 });
+
 
 
 app.post("/", function (req, res) {
@@ -74,6 +74,62 @@ app.post("/", function (req, res) {
 app.post("/new-serach", function(req, res){
     res.sendFile(__dirname + "/index.html");
 })
+
+app.post("/popular-cities", function(req, res){
+    let palce = "Kolkata"
+    const ApiKey = process.env.API_KEY;
+    let url = "https://api.openweathermap.org/data/2.5/weather?q="+palce+"&APPID="+ApiKey+"&units=metric"
+    https.get(url, function (response) {
+        response.on("data", function (data) {
+            let weatherData = JSON.parse(data)
+            let temp = weatherData.main.temp;
+            let feels_like = weatherData.main.feels_like;
+            let icon = weatherData.weather[0].icon;
+            let temp_min = weatherData.main.temp_min;
+            let temp_max = weatherData.main.temp_max;
+            let humidity=weatherData.main.humidity;
+            let pressure=weatherData.main.pressure;
+            let imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+
+        res.setHeader("Content-Type", "text/html");
+        res.write('<head><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet"></head>')
+        res.write("<style>*{margin: 0; padding: 0;font-family: 'Open Sans', sans-serif;}body{background: no-repeat url('gradient-bg.jpg'); background-repeat: no-repeat;background-size: cover;text-align: center; } </style>");
+        res.write("<style>header{text-align: center; font-size: 4rem; padding: 1rem;background: rgba(0, 247, 255, 0.123); box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );backdrop-filter: blur( 6px ); -webkit-backdrop-filter: blur( 6px );border-radius: 10px;color: black;font-weight: bolder; }</style>")
+        res.write("<style>.form{background: rgba( 255, 255, 255, 0.4 ); box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );backdrop-filter: blur( 6px );-webkit-backdrop-filter: blur( 6px );border-radius: 10px; max-width: 50rem; margin-left: auto;margin-right: auto;padding: 2rem; }.form h1{padding: 1rem 0rem}</style>")
+        res.write("<style>.main{width: 50vw;min-height: 49.55vh;text-align: center; justify-content: center;padding: 10rem 0;}</style>")
+        res.write("<style>.min-max{width: 25rem}.flex{display: flex;}h3{padding: 0.5rem; }footer{text-align: center;background-color: black; color: white; height: 5vh;line-height: 5vh; }</style>");
+        res.write("<style>.popular_city_flex{display: flex}.new_loc_btn form button{padding: 0.4rem;font-size: 1.25rem;background-color: rgba(0, 234, 255, 0.589);border-radius: 10px;border-color: transparent;font-weight: 900;cursor: pointer;}</style>")
+        res.write("<header> Weather App</header>");
+        res.write("<h1>Weather of Popular Cities.</h1>")
+        res.write("<div class='popular_city_flex'><div class='main'><div class='form'><h1>The temperature in "+palce+" is: " + temp + " &#8451;</h1><h3>The Temp Feels like: "+feels_like+"&#8451;</h3><h3>The Weather Currently is: " + weatherData.weather[0].description+ ". </h3><img src=" + imageURL + "><div class='flex'><div class='min-max'><h3>Min Temp: "+temp_min+ " &#8451;</h3></div><div class='min-max'><h3>Max Temp: "+temp_max+ " &#8451;</h3></div></div><div class='flex'><div class='min-max'><h3>Humidity: "+humidity+ "%</h3></div><div class='min-max'><h3>Presure: "+pressure+ "bar</h3></div></div>");
+        res.write("<div class='new_loc_btn'><form action='/new-serach' method='post'><button type='submit'>Search New Location</button></form></div></div></div>")
+        
+         palce = "London"
+         url = "https://api.openweathermap.org/data/2.5/weather?q="+palce+"&APPID="+ApiKey+"&units=metric"
+        https.get(url, function (response) {
+            response.on("data", function (data) {
+                let weatherData = JSON.parse(data)
+                let temp = weatherData.main.temp;
+                let feels_like = weatherData.main.feels_like;
+                let icon = weatherData.weather[0].icon;
+                let temp_min = weatherData.main.temp_min;
+                let temp_max = weatherData.main.temp_max;
+                let humidity=weatherData.main.humidity;
+                let pressure=weatherData.main.pressure;
+                let imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+
+                res.write("<div class='main'><div class='form'><h1>The temperature in "+palce+" is: " + temp + " &#8451;</h1><h3>The Temp Feels like: "+feels_like+"&#8451;</h3><h3>The Weather Currently is: " + weatherData.weather[0].description+ ". </h3><img src=" + imageURL + "><div class='flex'><div class='min-max'><h3>Min Temp: "+temp_min+ " &#8451;</h3></div><div class='min-max'><h3>Max Temp: "+temp_max+ " &#8451;</h3></div></div><div class='flex'><div class='min-max'><h3>Humidity: "+humidity+ "%</h3></div><div class='min-max'><h3>Presure: "+pressure+ "bar</h3></div></div>");
+        res.write("<div class='new_loc_btn'><form action='/new-serach' method='post'><button type='submit'>Search New Location</button></form></div></div></div></div>")
+        res.write("<footer>Copyright &copy; 2021 | Build by Rajdeep Sengupta with &#10084;.</footer>");
+        
+                
+        res.send();
+    })
+})
+})
+    })
+})
+
 
 
 app.listen(3000, function () {
