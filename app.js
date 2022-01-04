c80bd251dcd89b003e02bdd59ecf8727f6cbe7db
Rajdeep <rajdipgupta019@gmail>
2022-01-04 13:11:17 +0530

    heroku deploy added

--- app.js
+++ app.js
@@ -172,6 +172,6 @@ app.post("/popular-cities", function(req, res){
 
 
 
-app.listen(3000, function () {
+app.listen( process.env.PORT || 3000, function () {
     console.log("Server is Running........")
 })
